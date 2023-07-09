9f3aa3263c7caa7b16cdd2362a0658a2b9f3af8c
Janne Ukkonen <jataukko@jalava.cc.jyu.fi>
2023-07-09 23:08:31 +0300

    Variliukupalkkien varin muutos tapahtumakasittelijalla

--- vt4.js
+++ vt4.js
@@ -5,8 +5,8 @@ window.onload = function() {
     //let svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
     //svg.appendChild(rect);
     // Tässä esimerkki miten tehdä javascriptillä svg-kuvia: tee tapahtuman sisällä, muuten elementin haku ei onnistu.
-    // Referoi piirrettävään svg:hen id:n avulla. sitten tee createElementNS kutsulla haluttu muotu, sitten monta setAttributea
-    // että saat haluamasi kuvan
+    // Referoi piirrettävään svg:hen id:n avulla. sitten tee createElementNS kutsulla haluttu muotu, sitten muuta elementin
+    // style arvoja haluamaksesi
     let svg = document.getElementById("piirtoalusta");
     for (let i=0; i<10; i++) {
         let rect = document.createElementNS("http://www.w3.org/2000/svg","rect");
@@ -23,22 +23,21 @@ window.onload = function() {
         //rect.style.zIndex = -i;
         //rect.style.sijainti = i*3;
         rect.style.fill = "url(#Gradient1)";
+        rect.addEventListener("animationiteration", animlistener);
         svg.appendChild(rect);
     }
 
-    
-    
+    // Muutetaan palkkien väri tapahtumakäsittelijällä
+    function animlistener(e) {
+        if (e.target.style.fill == `url("#Gradient1")`) {
+            e.target.style.fill = "url(#Gradient2)";
+        }
+        else {
+            e.target.style.fill = "url(#Gradient1)";
+        }
+    }
     //svg.setAttribute("xmlns", "http://www.w3.org/2000/svg");
     //svg.setAttribute("version", "1.1");
     //svg.setAttribute("width", "300");
     //svg.setAttribute("height", "200");
-    /*
-    let rect = document.createElementNS("http://www.w3.org/2000/svg","rect");
-    svg.appendChild(rect);
-    rect.setAttribute("x", "0");
-    rect.setAttribute("y", "0");
-    rect.setAttribute("width", "50%");
-    rect.setAttribute("height", "50%");
-    rect.setAttribute("fill", 'red');
-    */
     };
